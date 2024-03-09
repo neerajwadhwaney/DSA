73b16ab5908825f84d4dc2df6f3859e41d04f8a1
neerajwadhwaney <neerajwadhwaney2003@gmail.com>
2024-03-09 22:49:39 +0530

    updated

---
+++ src/CodingNingaDSA/questions_leetcode/RemoveNNodeFromEnd.java
@@ -0,0 +1,34 @@
+package CodingNingaDSA.questions_leetcode;
+
+public class RemoveNNodeFromEnd {
+    public static class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
+
+    public static ListNode removeNthFromEnd(ListNode head, int n) {
+        int length = findLength(head);
+        int i = 0, traverseTill = length - n - 1;
+        if(traverseTill == -1) return head.next;
+        ListNode curr = head;
+        while(i < traverseTill){
+            curr = curr.next;
+            i++;
+        }
+        curr.next = curr.next.next;
+        return head;
+    }
+    public static int findLength(ListNode head){
+        int count = 0;
+        if(head == null) return count;
+        ListNode curr = head;
+        while(curr != null){
+            count++;
+            curr = curr.next;
+        }
+        return count;
+    }
+}
